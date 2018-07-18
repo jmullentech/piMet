13590fdd36dad3cf84892b2d7b9e644a0ee6d140
John Mullen <jmullentech@gmail.com>
2018-07-17 23:30:26 -0400

    More function overhauling

--- piMet.py
+++ piMet.py
@@ -19,7 +19,7 @@ class piMet:
 		self.http = httplib2.Http()
 		self.http = self.credentials.authorize(self.http)
 
-		# Interval in seconds between measurements.
+		# Interval in seconds between measurements
 		self.observationFreq = 15
 
 		# Altitude of station in meters
@@ -33,7 +33,7 @@ class piMet:
 			self.gc.login()
 			print("Refresh SUCCESSFUL!")
 
-	def calcs(self):
+	def readSensor(self):
 
 		# Get I2C bus
 		bus = smbus.SMBus(1)
@@ -41,40 +41,40 @@ class piMet:
 
 		# Convert the data
 		# Temp coefficents
-		dig_T1 = b1[1] * 256 + b1[0]
-		dig_T2 = b1[3] * 256 + b1[2]
-		if dig_T2 > 32767 :
-		    dig_T2 -= 65536
-		dig_T3 = b1[5] * 256 + b1[4]
-		if dig_T3 > 32767 :
-		    dig_T3 -= 65536
+		self.dig_T1 = b1[1] * 256 + b1[0]
+		self.dig_T2 = b1[3] * 256 + b1[2]
+		if self.dig_T2 > 32767 :
+		    self.dig_T2 -= 65536
+		self.dig_T3 = b1[5] * 256 + b1[4]
+		if self.dig_T3 > 32767 :
+		    self.dig_T3 -= 65536
 
 		# Pressure coefficents
-		dig_P1 = b1[7] * 256 + b1[6]
-		dig_P2 = b1[9] * 256 + b1[8]
-		if dig_P2 > 32767 :
-		    dig_P2 -= 65536
-		dig_P3 = b1[11] * 256 + b1[10]
-		if dig_P3 > 32767 :
-		    dig_P3 -= 65536
-		dig_P4 = b1[13] * 256 + b1[12]
-		if dig_P4 > 32767 :
-		    dig_P4 -= 65536
-		dig_P5 = b1[15] * 256 + b1[14]
-		if dig_P5 > 32767 :
-		    dig_P5 -= 65536
-		dig_P6 = b1[17] * 256 + b1[16]
-		if dig_P6 > 32767 :
-		    dig_P6 -= 65536
-		dig_P7 = b1[19] * 256 + b1[18]
-		if dig_P7 > 32767 :
-		    dig_P7 -= 65536
-		dig_P8 = b1[21] * 256 + b1[20]
-		if dig_P8 > 32767 :
-		    dig_P8 -= 65536
-		dig_P9 = b1[23] * 256 + b1[22]
-		if dig_P9 > 32767 :
-		    dig_P9 -= 65536
+		self.dig_P1 = b1[7] * 256 + b1[6]
+		self.dig_P2 = b1[9] * 256 + b1[8]
+		if self.dig_P2 > 32767 :
+		    self.dig_P2 -= 65536
+		self.dig_P3 = b1[11] * 256 + b1[10]
+		if self.dig_P3 > 32767 :
+		    self.dig_P3 -= 65536
+		self.dig_P4 = b1[13] * 256 + b1[12]
+		if self.dig_P4 > 32767 :
+		    self.dig_P4 -= 65536
+		self.dig_P5 = b1[15] * 256 + b1[14]
+		if self.dig_P5 > 32767 :
+		    self.dig_P5 -= 65536
+		self.dig_P6 = b1[17] * 256 + b1[16]
+		if self.dig_P6 > 32767 :
+		    self.dig_P6 -= 65536
+		self.dig_P7 = b1[19] * 256 + b1[18]
+		if self.dig_P7 > 32767 :
+		    self.dig_P7 -= 65536
+		self.dig_P8 = b1[21] * 256 + b1[20]
+		if self.dig_P8 > 32767 :
+		    self.dig_P8 -= 65536
+		self.dig_P9 = b1[23] * 256 + b1[22]
+		if self.dig_P9 > 32767 :
+		    self.dig_P9 -= 65536
 
 		# BMP280 address, 0x77(118)
 		# Select Control measurement register, 0xF4(244)
@@ -95,28 +95,29 @@ class piMet:
 		data = bus.read_i2c_block_data(0x77, 0xF7, 8)
 
 		# Convert pressure and temperature data to 19-bits
-		adc_p = ((data[0] * 65536) + (data[1] * 256) + (data[2] & 0xF0)) / 16
-		adc_t = ((data[3] * 65536) + (data[4] * 256) + (data[5] & 0xF0)) / 16
+		self.adc_p = ((data[0] * 65536) + (data[1] * 256) + (data[2] & 0xF0)) / 16
+		self.adc_t = ((data[3] * 65536) + (data[4] * 256) + (data[5] & 0xF0)) / 16
 
+	def calcs(self):
 		# Temperature offset calculations
-		var1 = ((adc_t) / 16384.0 - (dig_T1) / 1024.0) * (dig_T2)
-		var2 = (((adc_t) / 131072.0 - (dig_T1) / 8192.0) * ((adc_t)/131072.0 - (dig_T1)/8192.0)) * (dig_T3)
+		var1 = ((self.adc_t) / 16384.0 - (self.dig_T1) / 1024.0) * (self.dig_T2)
+		var2 = (((self.adc_t) / 131072.0 - (self.dig_T1) / 8192.0) * ((self.adc_t)/131072.0 - (self.dig_T1)/8192.0)) * (self.dig_T3)
 		t_fine = (var1 + var2)
 		self.cTemp = (var1 + var2) / 5120.0
 		self.fTemp = self.cTemp * 1.8 + 32
 
 		# Pressure offset calculations
 		var1 = (t_fine / 2.0) - 64000.0
-		var2 = var1 * var1 * (dig_P6) / 32768.0
-		var2 = var2 + var1 * (dig_P5) * 2.0
-		var2 = (var2 / 4.0) + ((dig_P4) * 65536.0)
-		var1 = ((dig_P3) * var1 * var1 / 524288.0 + ( dig_P2) * var1) / 524288.0
-		var1 = (1.0 + var1 / 32768.0) * (dig_P1)
-		p = 1048576.0 - adc_p
+		var2 = var1 * var1 * (self.dig_P6) / 32768.0
+		var2 = var2 + var1 * (self.dig_P5) * 2.0
+		var2 = (var2 / 4.0) + ((self.dig_P4) * 65536.0)
+		var1 = ((self.dig_P3) * var1 * var1 / 524288.0 + ( self.dig_P2) * var1) / 524288.0
+		var1 = (1.0 + var1 / 32768.0) * (self.dig_P1)
+		p = 1048576.0 - self.adc_p
 		p = (p - (var2 / 4096.0)) * 6250.0 / var1
-		var1 = (dig_P9) * p * p / 2147483648.0
-		var2 = p * (dig_P8) / 32768.0
-		self.pressure = (p + (var1 + var2 + (dig_P7)) / 16.0) / 100
+		var1 = (self.dig_P9) * p * p / 2147483648.0
+		var2 = p * (self.dig_P8) / 32768.0
+		self.pressure = (p + (var1 + var2 + (self.dig_P7)) / 16.0) / 100
 
 		# Adjust station pressure to MSLP
 		self.mslp = (self.pressure + (self.alt / 9.2)) * 0.02953
@@ -141,6 +142,7 @@ class piMet:
 
 	def runall(self):
 		while True:
+			runall.readSensor()
 			runall.calcs()
 			runall.output()
 			runall.renegCreds()
